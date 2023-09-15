b43ea73692e317587b5c860829b8694bbf15b815
Pablo Palma Rodríguez <pablopalmarodriguez@MacBook-Air-de-Pablo.local>
2023-09-15 10:05:02 +0200

    3_lst_updated

--- Makefile
+++ Makefile
@@ -5,9 +5,10 @@ SRC	=	ft_atoi.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c \
 		ft_strnstr.c ft_strrchr.c ft_strlcpy.c ft_strlcat.c ft_toupper.c \
 		ft_tolower.c ft_strdup.c ft_calloc.c ft_substr.c ft_strjoin.c \
 		ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c \
-		ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c
+		ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 OBJ = $(SRC:.c=.o)
-BONUS_SRC =	ft_lstnew.c ft_lstadd_front.c
+BONUS_SRC =	ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c \
+			ft_lstadd_back.c ft_lstdelone.c
 BONUS_OBJ = $(BONUS_SRC:.c=.o)
 FLAGS =	-Wall -Werror -Wextra
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pabpalma <pabpalma>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 17:24:17 by pabpalma          #+#    #+#             */
-/*   Updated: 2023/09/13 20:09:42 by pabpalma         ###   ########.fr       */
+/*   Updated: 2023/09/15 09:26:07 by pabpalma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ typedef struct s_list
 //List operations
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 // Conversions
 int		ft_atoi(const char *str);
@@ -75,5 +79,6 @@ int		ft_tolower(int c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
